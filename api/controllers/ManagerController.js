5cbafaa5d10f42acb985b7ebf71aaa6847ef36e9
unknown <aksenov007@LPR257.main.rosevrobank.ru>
2015-09-11 09:47:30 +0300

    Simple user adding and viewing form

---
+++ api/controllers/ManagerController.js
@@ -0,0 +1,13 @@
+/**
+ * ManagerController
+ *
+ * @description :: Server-side logic for managing Managers
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+    main: function(req, res) {
+        return res.view('manager');
+    }
+
+};
