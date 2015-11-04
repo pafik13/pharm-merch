9aac0ed1adff42c7dd2dbebd8f3e83d1d04566c7
anatoliyaksenov <anatoliy@aksenov.org>
2015-11-04 07:48:00 +0000

    DrugInfoType Model

---
+++ api/controllers/DrugInfoTypeController.js
@@ -0,0 +1,10 @@
+/**
+ * DrugInfoTypeController
+ *
+ * @description :: Server-side logic for managing Druginfotypes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+};
--- api/models/Attendance_image.js
+++ api/models/Attendance_image.js
@@ -3,6 +3,8 @@
  *
  * @description :: Фотографии с посещения
  * @docs        :: http://sailsjs.org/#!documentation/models
+ * @example     :: curl -X POST -d "identifier=aksenov&password=q1234567" -c test.txt localhost:8080/auth/local
+ *                 curl -b test.txt --form "drugImage=@mars.jpg" localhost:8080/Attendance_image/create
  */
 
 module.exports = {
