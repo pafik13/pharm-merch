3f54c816e6ef1583cf1b91901b844d8e04a59bde
pafik13 <re1ax_88@mail.ru>
2016-03-27 03:35:20 +0300

    GPSPoint
    Add GPSPoints

--- api/models/AttendanceGPSPoint.js
+++ api/models/AttendanceGPSPoint.js
@@ -1,5 +1,5 @@
 /**
- * ValueType.js
+ * AttendanceGPSPoint.js
  *
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/#!documentation/models
@@ -8,6 +8,13 @@
 module.exports = {
 
     attributes: {
-        name: 'string'
+        attendance: {
+            model: 'Attendance',
+            required: true
+        },
+        longitude: 'float',
+        latitude: 'float',
+        provider: 'string',
+        stamp: 'datetime'
     }
 };
